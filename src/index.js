c8604a503fddecb467ff9ed6583bd71795904848
Westin Schepper <westin@teeps.org>
2016-10-11 23:40:04 -0400

    Implements routes for reading data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import App from './containers/App'
+import Landing from './containers/Landing'
+import ProjectContainer from './containers/Project'
+import MemberContainer from './containers/Member'
+import StandupContainer from './containers/Standup'
+
+import NotFound from './components/not-found'
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={Landing} />
+      <Route path='projects/:id' component={ProjectContainer} />
+      <Route path='members/:id' component={MemberContainer} />
+      <Route path='standups/:id' component={StandupContainer} />
+      <Route path='*' component={NotFound} />
+    </Route>
+  </Router>
+  ), document.getElementById('root')
+)
