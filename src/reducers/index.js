0a29e5521f3f8dc34cc8a289a2142720820b9404
Westin Schepper <westin@teeps.org>
2017-05-22 12:36:33 -0400

    Add redux-form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
+import { reducer as formReducer } from 'redux-form'
 import entities from './entities'
 
 export default combineReducers({
   entities,
-  routing: routerReducer
+  routing: routerReducer,
+  form: formReducer
 })
