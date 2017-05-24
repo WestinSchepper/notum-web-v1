1311972159741dbea28d75272e929593c855bd95
Westin Schepper <westin@teeps.org>
2017-05-24 08:18:49 -0400

    Add email field to member detail

--- src/components/member-detail/index.js
+++ src/components/member-detail/index.js
@@ -2,9 +2,10 @@ import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 import moment from 'moment'
 
-const MemberDetail = ({id, name, createdAt, updatedAt}) => (
+const MemberDetail = ({id, name, email, createdAt, updatedAt}) => (
   <div>
     <h2>{name} <Link to={`/members/${id}/edit`}><button>edit</button></Link></h2>
+    <h3>{email}</h3>
     <ul>
       <li>Member ID: {id}</li>
       <li>Joined: {moment(createdAt).format('MMMM do, YYYY')}</li>
@@ -16,6 +17,7 @@ const MemberDetail = ({id, name, createdAt, updatedAt}) => (
 MemberDetail.propTypes = {
   id: PropTypes.number,
   name: PropTypes.string,
+  email: PropTypes.string,
   createdAt: PropTypes.string,
   updatedAt: PropTypes.string
 }
